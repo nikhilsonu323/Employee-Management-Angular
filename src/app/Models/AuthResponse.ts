f805ba4655d76175db546bfa6f07575e357ba410
nikhil <nikhilsonu323@gmail.com>
2024-07-24 06:14:13 +0000

    Auto-generated commit
    Triggered by commit: https://github.com/nikhilsonu323/Employee-Management-Angular/commit/ed985e61e302647fddd26d85c0f3eec3ee949ad1

--- src/app/Models/AuthResponse.ts
+++
@@ -1,8 +0,0 @@
-
-export interface AuthResponse{
-    name: string;
-    email: string;
-    imageData: string;
-    token: string;
-    expiresIn: number;
-}
--- src/app/Models/FilterContent.ts
+++
@@ -1,5 +0,0 @@
-export class FilterContent{
-    status: Set<number> = new Set();
-    departments: Set<number> = new Set();
-    location: Set<number> = new Set();
-}
